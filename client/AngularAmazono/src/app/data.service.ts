5588753df25f5fb05df09a36307039d3c476de19
BakerAO <dpshunter@gmail.com>
2018-04-05 10:30:20 -0500

    added more login stuff

--- client/AngularAmazono/src/app/data.service.ts
+++ client/AngularAmazono/src/app/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { NavigationStart, Router } from '@angular/router';
+import { RestApiService } from './rest-api.service';
 
 @Injectable()
 export class DataService {
@@ -7,7 +8,7 @@ export class DataService {
   messageType = 'danger';
   user: any;
 
-  constructor(private router: Router) { 
+  constructor(private router: Router, private rest: RestApiService) { 
     this.router.events.subscribe(event => {
       if (event instanceof NavigationStart) {
         this.message = '';
@@ -28,5 +29,18 @@ export class DataService {
   warning(message){
     this.messageType = 'warning';
     this.message = message;
+  }
+
+  async getProfile() {
+    try {
+      if (localStorage.getItem('token')) {
+        const data = await this.rest.get(
+          'http://localhost:3030/api/accounts/profile'
+        );
+        this.user = data['user'];
+      }
+    } catch (error) {
+      this.error(error);
+    }
   }
 }
