5bc1399c76774cfe3849b1469b3a9c79cee68bb1
BakerAO <dpshunter@gmail.com>
2018-04-19 18:01:57 -0500

    more products

--- client/AngularAmazono/src/app/product/product.component.ts
+++ client/AngularAmazono/src/app/product/product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataService } from '../data.service';
+import { RestApiService } from '../rest-api.service';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+  product: any;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private data: DataService,
+    private rest: RestApiService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(res => {
+      this.rest.get(`http://localhost:3030/api/product/${res['id']}`)
+        .then(data => {
+          data['success']
+            ? (this.product = data['product'])
+            : this.router.navigate(['/'])
+          ;
+        }).catch(error => this.data.error(error['message']));
+    });
   }
 
 }
