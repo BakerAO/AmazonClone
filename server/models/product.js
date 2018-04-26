ab06fa81474b99739cf82d722fc5ca7a21382b6d
BakerAO <dpshunter@gmail.com>
2018-04-26 16:07:34 -0500

    Algolia

--- server/models/product.js
+++ server/models/product.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongooseAlgolia = require('mongoose-algolia');
 const deepPopulate = require('mongoose-deep-populate')(mongoose);
-//const mongooseAlgolia = require('mongoose-algolia');
+const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema(
     {
@@ -37,24 +37,33 @@ ProductSchema
 ;
 
 ProductSchema.plugin(deepPopulate);
-// ProductSchema.plugin(mongooseAlgolia, {
-//     appId: 'KV5XNDATQL',
-//     apiKey: '864440e1986a9cfd1244d20aa2ad0619',
-//     indexName: 'amazonov1',
-//     selector: '',
-//     populate: {
-//         path: '',
-//         select: ''
-//     },
-//     defaults: {
 
-//     },
-//     mappings: {
+ProductSchema.plugin(mongooseAlgolia, {
+    appId: 'KV5XNDATQL',
+    apiKey: '864440e1986a9cfd1244d20aa2ad0619',
+    indexName: 'amazonov1',
+    selector: '_id title image reviews description price owner created averageRating',
+    populate: {
+        path: 'owner reviews',
+        select: 'name rating'
+    },
+    defaults: {
+        author: 'unknown'
+    },
+    mappings: {
+        title: function(value) {
+            return `${value}`;
+        }
+    },
+    virtuals: {
+    },
+    debug: true
+});
 
-//     },
-//     virtuals: {
+let Model = mongoose.model('Product', ProductSchema);
+Model.SyncToAlgolia();
+Model.SetAlgoliaSettings({
+    searchableAttributes: ['title', 'owner']
+});
 
-//     }
-// });
-
-module.exports = mongoose.model('Product', ProductSchema);
+module.exports = Model;
